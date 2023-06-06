c666d5664c739c45ebd639662198a8f9a5463870
Hamza Shaukat <75229817+hamzashaukat111@users.noreply.github.com>
2023-06-06 21:06:24 +0500

    working now

--- src/screens/Scan.js
+++ src/screens/Scan.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { View, Button, Image, StyleSheet } from "react-native";
+import { View, Button, Image, StyleSheet, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 const Scan = () => {
   const [image, setImage] = useState(null);
+  const [foodName, setFoodName] = useState(null);
 
   const pickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -64,12 +65,22 @@ const Scan = () => {
     // Process the response from Azure Custom Vision
     // Update the UI accordingly based on the predictions
     console.log(data);
+    const predictions = data.predictions;
+    const highestProbabilityTag = predictions.reduce(
+      (acc, curr) => (curr.probability > acc.probability ? curr : acc),
+      predictions[0]
+    );
+    const highestProbabilityTagName = highestProbabilityTag.tagName;
+
+    // Set the tagName to the component state
+    setFoodName(highestProbabilityTagName);
   };
 
   return (
     <View style={styles.container}>
       <Button title="Pick Image" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={styles.image} />}
+      {foodName && <Text style={styles.foodText}>The food is {foodName}</Text>}
     </View>
   );
 };
@@ -83,6 +94,11 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+    marginTop: 20,
+  },
+  foodText: {
+    fontSize: 24,
+    fontWeight: "bold",
     marginTop: 20,
   },
 });
